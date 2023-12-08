b302c94ca008b1f6d8a3965c92e55b156ac892a9
Michael Harvey <michael.harvey@goaptive.com>
2023-12-08 10:06:10 -0700

    add rest of day templates

---
+++ day24/src/bin/part2.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = include_str!("./input.txt");
+    let output = process(input);
+    dbg!(output);
+}
+
+/**
+
+*/
+fn process(input: &str) -> String {
+    let mut lines = input.lines();
+
+    return 0.to_string();
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::process;
+
+    #[test]
+    fn it_works() {
+        let result = process("");
+        assert_eq!(result, "".to_string());
+    }
+}
