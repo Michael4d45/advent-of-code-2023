c7e9ed0174de15c8f6dcdb034751af5e6dcd7a31
Michael Harvey <michael4d45@gmail.com>
2023-12-12 00:05:07 -0700

    set up test for day 12

--- day12/src/bin/part1.rs
+++ day12/src/bin/part1.rs
@@ -9,42 +9,42 @@ fn main() {
 /**
 --- Day 12: Hot Springs ---
 
-You finally reach the hot springs! You can see steam rising from secluded 
+You finally reach the hot springs! You can see steam rising from secluded
 areas attached to the primary, ornate building.
 
-As you turn to enter, the researcher stops you. "Wait - I thought you were 
+As you turn to enter, the researcher stops you. "Wait - I thought you were
 looking for the hot springs, weren't you?" You indicate that this definitely looks like hot springs to you.
 
 "Oh, sorry, common mistake! This is actually the onsen! The hot springs are next door."
 
-You look in the direction the researcher is pointing and suddenly notice the 
+You look in the direction the researcher is pointing and suddenly notice the
 massive metal helixes towering overhead. "This way!"
 
-It only takes you a few more steps to reach the main gate of the massive fenced-off 
+It only takes you a few more steps to reach the main gate of the massive fenced-off
 area containing the springs. You go through the gate and into a small administrative building.
 
-"Hello! What brings you to the hot springs today? Sorry they're not very hot right now; 
+"Hello! What brings you to the hot springs today? Sorry they're not very hot right now;
 we're having a lava shortage at the moment." You ask about the missing machine parts for Desert Island.
 
-"Oh, all of Gear Island is currently offline! Nothing is being manufactured at the moment, 
+"Oh, all of Gear Island is currently offline! Nothing is being manufactured at the moment,
 not until we get more lava to heat our forges. And our springs. The springs aren't very springy unless they're hot!"
 
-"Say, could you go up and see why the lava stopped flowing? The springs are too cold 
+"Say, could you go up and see why the lava stopped flowing? The springs are too cold
 for normal operation, but we should be able to find one springy enough to launch you up there!"
 
-There's just one problem - many of the springs have fallen into disrepair, so they're 
-not actually sure which springs would even be safe to use! Worse yet, their condition records 
+There's just one problem - many of the springs have fallen into disrepair, so they're
+not actually sure which springs would even be safe to use! Worse yet, their condition records
 of which springs are damaged (your puzzle input) are also damaged! You'll need to help them repair the damaged records.
 
-In the giant field just outside, the springs are arranged into rows. For each row, the 
-condition records show every spring and whether it is operational (.) or damaged (#). 
+In the giant field just outside, the springs are arranged into rows. For each row, the
+condition records show every spring and whether it is operational (.) or damaged (#).
 This is the part of the condition records that is itself damaged; for some springs, it is simply unknown (?) whether the spring is operational or damaged.
 
-However, the engineer that produced the condition records also duplicated some of this 
-information in a different format! After the list of springs for a given row, the size 
-of each contiguous group of damaged springs is listed in the order those groups appear 
-in the row. This list always accounts for every damaged spring, and each number is the 
-entire size of its contiguous group (that is, groups are always separated by at least 
+However, the engineer that produced the condition records also duplicated some of this
+information in a different format! After the list of springs for a given row, the size
+of each contiguous group of damaged springs is listed in the order those groups appear
+in the row. This list always accounts for every damaged spring, and each number is the
+entire size of its contiguous group (that is, groups are always separated by at least
     one operational spring: #### would always be 4, never 2,2).
 
 So, condition records with no unknown spring conditions might look like this:
@@ -56,7 +56,7 @@ So, condition records with no unknown spring conditions might look like this:
 #....######..#####. 1,6,5
 .###.##....# 3,2,1
 
-However, the condition records are partially damaged; some of the springs' conditions are 
+However, the condition records are partially damaged; some of the springs' conditions are
 actually unknown (?). For example:
 
 ???.### 1,1,3
@@ -66,20 +66,20 @@ actually unknown (?). For example:
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1
 
-Equipped with this information, it is your job to figure out how many different arrangements of 
+Equipped with this information, it is your job to figure out how many different arrangements of
 operational and broken springs fit the given criteria in each row.
 
-In the first line (???.### 1,1,3), there is exactly one way separate groups of one, one, and 
+In the first line (???.### 1,1,3), there is exactly one way separate groups of one, one, and
 three broken springs (in that order) can appear in that row: the first three unknown springs must be broken, then operational, then broken (#.#), making the whole row #.#.###.
 
-The second line is more interesting: .??..??...?##. 1,1,3 could be a total of four different 
-arrangements. The last ? must always be broken (to satisfy the final contiguous group of three broken springs), 
-and each ?? must hide exactly one of the two broken springs. (Neither ?? could be both broken springs or they 
-    would form a single contiguous group of two; if that were true, the numbers afterward would have been 2,3 instead.) 
+The second line is more interesting: .??..??...?##. 1,1,3 could be a total of four different
+arrangements. The last ? must always be broken (to satisfy the final contiguous group of three broken springs),
+and each ?? must hide exactly one of the two broken springs. (Neither ?? could be both broken springs or they
+    would form a single contiguous group of two; if that were true, the numbers afterward would have been 2,3 instead.)
     Since each ?? can either be #. or .#, there are four possible arrangements of springs.
 
-The last line is actually consistent with ten different arrangements! Because the first number is 3, 
-the first and second ? must both be . (if either were #, the first number would have to be 4 or higher). 
+The last line is actually consistent with ten different arrangements! Because the first number is 3,
+the first and second ? must both be . (if either were #, the first number would have to be 4 or higher).
 However, the remaining run of unknown spring conditions have many different ways they could hold groups of two and one broken springs:
 
 ?###???????? 3,2,1
@@ -105,29 +105,91 @@ In this example, the number of possible arrangements for each row is:
 
 Adding all of the possible arrangement counts together produces a total of 21 arrangements.
 
-For each row, count all of the different arrangements of operational and broken springs that meet the given criteria. 
+For each row, count all of the different arrangements of operational and broken springs that meet the given criteria.
 What is the sum of those counts?
 
 */
+#[derive(Debug)]
+enum Spring {
+    Operational,
+    Broken,
+    Unknown,
+}
+
+#[derive(Debug)]
+struct Row {
+    springs: Vec<Spring>,
+    broken_groups: Vec<usize>,
+}
+
+fn get_spring(c: char) -> Spring {
+    use Spring::*;
+
+    match c {
+        '?' => Unknown,
+        '#' => Broken,
+        '.' => Operational,
+        _ => unreachable!("This was bad input {}", c),
+    }
+}
+
+fn get_row(input: &str) -> Row {
+    let temp: Vec<_> = input.split(" ").collect();
+    let temp_springs = temp[0];
+    let temp_groups = temp[1];
+
+    let springs = temp_springs.chars().map(get_spring).collect::<Vec<_>>();
+    let broken_groups = temp_groups
+        .split(",")
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+
+    Row {
+        springs,
+        broken_groups,
+    }
+}
+
 fn process(input: &str) -> String {
-    let mut lines = input.lines();
+    let rows = input.lines().map(get_row);
+
+    dbg!(rows.collect::<Vec<_>>());
 
     return 0.to_string();
 }
 
+fn get_count(row: Row) -> usize {
+    todo!();
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::process;
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[case("???.### 1,1,3", 1)]
+    #[case(".??..??...?##. 1,1,3", 4)]
+    #[case("?#?#?#?#?#?#?#? 1,3,1,6", 1)]
+    #[case("????.#...#... 4,1,1", 1)]
+    #[case("????.######..#####. 1,6,5", 4)]
+    #[case("?###???????? 3,2,1", 10)]
+    #[test]
+    fn get_count_works(#[case] input: &str, #[case] output: usize) {
+        let result = get_count(get_row(input));
+        assert_eq!(result, output);
+    }
 
     #[test]
     fn it_works() {
         let result = process(
-            "#.#.### 1,1,3
-.#...#....###. 1,1,3
-.#.###.#.###### 1,3,1,6
-####.#...#... 4,1,1
-#....######..#####. 1,6,5
-.###.##....# 3,2,1",
+            "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1",
         );
         assert_eq!(result, "21".to_string());
     }
