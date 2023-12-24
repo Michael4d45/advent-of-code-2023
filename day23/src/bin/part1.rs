d8b3476b441ada4ee7fdf23d4c5ba1e88c005acc
Michael Harvey <michael.harvey@goaptive.com>
2023-12-24 09:44:31 -0600

    complete day 23

--- day23/src/bin/part1.rs
+++ day23/src/bin/part1.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{BTreeMap, BTreeSet, HashMap};
 
 fn main() {
     let input = include_str!("./input.txt");
@@ -7,12 +7,184 @@ fn main() {
 }
 
 /**
+--- Day 23: A Long Walk ---
+
+The Elves resume water filtering operations!
+Clean water starts flowing over the edge of Island Island.
+
+They offer to help you go over the edge of Island Island, too!
+Just hold on tight to one end of this impossibly long rope and
+they'll lower you down a safe distance from the massive
+waterfall you just created.
+
+As you finally reach Snow Island, you see that the water isn't
+really reaching the ground: it's being absorbed by the air itself.
+It looks like you'll finally have a little downtime while the
+moisture builds up to snow-producing levels. Snow Island is pretty
+scenic, even without any snow; why not take a walk?
+
+There's a map of nearby hiking trails (your puzzle input) that
+indicates paths (.), forest (#), and steep slopes (^, >, v, and <).
+
+For example:
+
+#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#
+
+You're currently on the single path tile in the top row; your
+goal is to reach the single path tile in the bottom row. Because
+of all the mist from the waterfall, the slopes are probably
+quite icy; if you step onto a slope tile, your next step must
+be downhill (in the direction the arrow is pointing). To make
+sure you have the most scenic hike possible, never step onto
+the same tile twice. What is the longest hike you can take?
+
+In the example above, the longest hike you can take is
+marked with O, and your starting position is marked S:
+
+#S#####################
+#OOOOOOO#########...###
+#######O#########.#.###
+###OOOOO#OOO>.###.#.###
+###O#####O#O#.###.#.###
+###OOOOO#O#O#.....#...#
+###v###O#O#O#########.#
+###...#O#O#OOOOOOO#...#
+#####.#O#O#######O#.###
+#.....#O#O#OOOOOOO#...#
+#.#####O#O#O#########v#
+#.#...#OOO#OOO###OOOOO#
+#.#.#v#######O###O###O#
+#...#.>.#...>OOO#O###O#
+#####v#.#.###v#O#O###O#
+#.....#...#...#O#O#OOO#
+#.#########.###O#O#O###
+#...###...#...#OOO#O###
+###.###.#.###v#####O###
+#...#...#.#.>.>.#.>O###
+#.###.###.#.###.#.#O###
+#.....###...###...#OOO#
+#####################O#
+
+This hike contains 94 steps.
+(The other possible hikes you could have taken were 90, 86, 82, 82, and 74 steps long.)
+
+Find the longest hike you can take through the hiking
+trails listed on your map. How many steps long is the longest hike?
 
 */
-fn process(input: &str) -> String {
-    let mut lines = input.lines();
+enum Tile {
+    Forest,
+    Path,
+    Slope((usize, usize)),
+}
 
-    return 0.to_string();
+struct Maze {
+    maze: Vec<Vec<Tile>>,
+    x_size: usize,
+    y_size: usize,
+}
+
+impl Maze {
+    fn new(input: &str) -> Maze {
+        let mut maze = Vec::new();
+        let mut x_size = 0;
+        let mut y_size = 0;
+
+        for (y, line) in input.lines().enumerate() {
+            y_size = y;
+            let mut row = Vec::new();
+            for (x, c) in line.chars().enumerate() {
+                x_size = x;
+                row.push(match c {
+                    '>' => Tile::Slope((x + 1, y)),
+                    '<' => Tile::Slope((x - 1, y)),
+                    'v' => Tile::Slope((x, y + 1)),
+                    '^' => Tile::Slope((x, y - 1)),
+                    '.' => Tile::Path,
+                    '#' => Tile::Forest,
+                    _ => unreachable!("Bad input: {c}"),
+                });
+            }
+            maze.push(row);
+        }
+
+        Maze {
+            maze,
+            x_size,
+            y_size,
+        }
+    }
+
+    fn walk_tile(
+        &self,
+        visited: BTreeSet<(usize, usize)>,
+        x: usize,
+        y: usize,
+        result: &mut Vec<usize>,
+    ) {
+        if x == self.x_size - 1 && y == self.y_size {
+            result.push(visited.len());
+
+            return;
+        }
+        if visited.contains(&(x, y)) {
+            return;
+        }
+        let mut visited = visited;
+        visited.insert((x, y));
+        match self.maze[y][x] {
+            Tile::Path => {
+                self.walk_tile(visited.clone(), x + 1, y, result);
+                if x > 0 {
+                    self.walk_tile(visited.clone(), x - 1, y, result);
+                }
+                self.walk_tile(visited.clone(), x, y + 1, result);
+                if y > 0 {
+                    self.walk_tile(visited.clone(), x, y - 1, result)
+                }
+            }
+            Tile::Slope((to_x, to_y)) => self.walk_tile(visited, to_x, to_y, result),
+            _ => (),
+        }
+    }
+
+    fn walk(&self) -> Vec<usize> {
+        let mut result = Vec::new();
+        self.walk_tile(BTreeSet::new(), 1, 0, &mut result);
+        result
+    }
+}
+
+fn process(input: &str) -> String {
+    let mut maze = Maze::new(input);
+
+    let mut results = maze.walk();
+    
+    results.sort();
+
+    return results.last().unwrap().to_string();
 }
 
 #[cfg(test)]
@@ -21,9 +193,30 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = process(
-            "",
-        );
-        assert_eq!(result, "".to_string());
+        let input = "#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#";
+        let result = process(input);
+        assert_eq!(result, "94".to_string());
     }
 }
--- day23/src/bin/part2.rs
+++ day23/src/bin/part2.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{BTreeMap, BTreeSet, HashMap};
 
 fn main() {
     let input = include_str!("./input.txt");
@@ -7,21 +7,270 @@ fn main() {
 }
 
 /**
+--- Part Two ---
+
+As you reach the trailhead, you realize that the
+ground isn't as slippery as you expected; you'll
+have no problem climbing up the steep slopes.
+
+Now, treat all slopes as if they were normal paths (.).
+You still want to make sure you have the most scenic hike possible,
+so continue to ensure that you never step onto the same tile twice.
+What is the longest hike you can take?
+
+In the example above, this increases the longest hike to 154 steps:
+
+#S#####################
+#OOOOOOO#########OOO###
+#######O#########O#O###
+###OOOOO#.>OOO###O#O###
+###O#####.#O#O###O#O###
+###O>...#.#O#OOOOO#OOO#
+###O###.#.#O#########O#
+###OOO#.#.#OOOOOOO#OOO#
+#####O#.#.#######O#O###
+#OOOOO#.#.#OOOOOOO#OOO#
+#O#####.#.#O#########O#
+#O#OOO#...#OOO###...>O#
+#O#O#O#######O###.###O#
+#OOO#O>.#...>O>.#.###O#
+#####O#.#.###O#.#.###O#
+#OOOOO#...#OOO#.#.#OOO#
+#O#########O###.#.#O###
+#OOO###OOO#OOO#...#O###
+###O###O#O###O#####O###
+#OOO#OOO#O#OOO>.#.>O###
+#O###O###O#O###.#.#O###
+#OOOOO###OOO###...#OOO#
+#####################O#
+
+Find the longest hike you can take through the surprisingly dry hiking
+trails listed on your map. How many steps long is the longest hike?
 
 */
-fn process(input: &str) -> String {
-    let mut lines = input.lines();
+#[derive(PartialEq, Eq)]
+enum Tile {
+    Forest,
+    Path,
+}
+struct Maze {
+    maze: Vec<Vec<Tile>>,
+    x_size: usize,
+    y_size: usize,
+    cache: BTreeMap<(usize, usize), BTreeSet<(usize, usize)>>,
+}
 
-    return 0.to_string();
+impl Maze {
+    fn new(input: &str) -> Maze {
+        let mut maze = Vec::new();
+        let mut x_size = 0;
+        let mut y_size = 0;
+
+        for (y, line) in input.lines().enumerate() {
+            y_size = y;
+            let mut row = Vec::new();
+            for (x, c) in line.chars().enumerate() {
+                x_size = x;
+                row.push(match c {
+                    '>' => Tile::Path,
+                    '<' => Tile::Path,
+                    'v' => Tile::Path,
+                    '^' => Tile::Path,
+                    '.' => Tile::Path,
+                    '#' => Tile::Forest,
+                    _ => unreachable!("Bad input: {c}"),
+                });
+            }
+            maze.push(row);
+        }
+
+        Maze {
+            maze,
+            x_size,
+            y_size,
+            cache: BTreeMap::new(),
+        }
+    }
+
+    fn walk_tile(
+        &mut self,
+        x: usize,
+        y: usize,
+        traversed: BTreeSet<(usize, usize)>,
+    ) -> Option<BTreeSet<(usize, usize)>> {
+        if x == self.x_size - 1 && y == self.y_size {
+            dbg!(traversed.len(), &self.cache.len());
+            return Some(BTreeSet::new());
+        }
+        if self.maze[y][x] != Tile::Path {
+            return None;
+        }
+        if traversed.contains(&(x, y)) {
+            return None;
+        }
+        if let Some(&ref cache) = self.cache.get(&(x, y)) {
+            if cache.len() > traversed.len() {
+                return None;
+            }
+            // return cache.clone();
+        }
+        let mut traversed = traversed;
+        traversed.insert((x, y));
+        let mut max = None;
+        if let Some(mut back_track) = self.walk_tile(x + 1, y, traversed.clone()) {
+            if back_track.insert((x + 1, y)) {
+                max = Some(back_track);
+            }
+        }
+        if let Some(mut back_track) = self.walk_tile(x, y + 1, traversed.clone()) {
+            if back_track.insert((x, y + 1)) {
+                if let Some(cur_max) = &max {
+                    if back_track.len() > cur_max.len() {
+                        max = Some(back_track);
+                    }
+                } else {
+                    max = Some(back_track);
+                }
+            }
+        }
+        if x > 0 {
+            if let Some(mut back_track) = self.walk_tile(x - 1, y, traversed.clone()) {
+                if back_track.insert((x - 1, y)) {
+                    if let Some(cur_max) = &max {
+                        if back_track.len() > cur_max.len() {
+                            max = Some(back_track);
+                        }
+                    } else {
+                        max = Some(back_track);
+                    }
+                }
+            }
+        }
+        if y > 0 {
+            if let Some(mut back_track) = self.walk_tile(x, y - 1, traversed.clone()) {
+                if back_track.insert((x, y - 1)) {
+                    if let Some(cur_max) = &max {
+                        if back_track.len() > cur_max.len() {
+                            max = Some(back_track);
+                        }
+                    } else {
+                        max = Some(back_track);
+                    }
+                }
+            }
+        }
+        // if let Some(cache) = &max {
+        //     dbg!(cache.len());
+        //     for py in 0..=self.y_size {
+        //         for px in 0..=self.x_size {
+        //             match self.maze[py][px] {
+        //                 Tile::Forest => print!("█"),
+        //                 Tile::Path => {
+        //                     if px == x && py == y {
+        //                         print!("O");
+        //                     } else if cache.contains(&(px, py)) {
+        //                         print!(".");
+        //                     } else if traversed.contains(&(px, py)) {
+        //                         print!(",")
+        //                     } else {
+        //                         print!(" ")
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //         print!("\n");
+        //     }
+            // self.cache.insert((x, y), traversed.clone());
+        // }
+
+        max
+    }
+
+    fn walk(&mut self) -> usize {
+        self.cache.clear();
+        let walk = self.walk_tile(1, 0, BTreeSet::new());
+
+        // for ((x, y), c) in &self.cache {
+        if let Some(cache) = walk {
+            dbg!(cache.len());
+            for y in 0..=self.y_size {
+                for x in 0..=self.x_size {
+                    match self.maze[y][x] {
+                        Tile::Forest => print!("█"),
+                        Tile::Path => {
+                            if cache.contains(&(x, y)) {
+                                print!(".");
+                            } else {
+                                print!(" ")
+                            }
+                        }
+                    }
+                }
+                print!("\n");
+            }
+        }
+        // }
+
+        0
+    }
+}
+
+fn process(input: &str) -> String {
+    let mut maze = Maze::new(input);
+
+    return maze.walk().to_string();
 }
 
 #[cfg(test)]
 mod tests {
     use crate::process;
 
-    #[test]
-    fn it_works() {
-        let result = process("");
-        assert_eq!(result, "".to_string());
-    }
+    //     #[test]
+    //     fn it_works_small() {
+    //         let input = "#.####
+    // #....#
+    // #....#
+    // #....#
+    // ####.#";
+    //         let result = process(input);
+    //         assert_eq!(result, "_".to_string());
+    //     }
+//     #[test]
+//     fn it_works_small() {
+//         let input = "#.#####
+// #.....#
+// #.....#
+// #####.#";
+//         let result = process(input);
+//         assert_eq!(result, "_".to_string());
+//     }
+
+        #[test]
+        fn it_works() {
+            let input = "#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#";
+            let result = process(input);
+            assert_eq!(result, "_".to_string());
+        }
 }
